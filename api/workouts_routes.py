1cab4a84226438b8d428906d322256766ab993d7
EC2 Default User <ec2-user@ip-172-31-18-126.us-east-2.compute.internal>
2023-12-06 08:19:41 +0000

    update workouts routes and create new test cases

--- api/workouts_routes.py
+++ api/workouts_routes.py
@@ -5,15 +5,28 @@ from extensions import db
 
 workouts_blueprint = Blueprint('workouts', __name__)
 
-@workouts_blueprint.route('/api/workouts', methods=['GET'])
+@workouts_blueprint.route('/api/getworkouts', methods=['GET'])
 def get_workouts():
     # Retrieve all workouts
     workouts = Workout.query.all()
     return jsonify([workout.serialize() for workout in workouts])
+    
+@workouts_blueprint.route('/api/getworkout/<int:workout_id>', methods=['GET'])
+def get_workout(workout_id):
+    print(f"Received request for workout ID: {workout_id}")
+    workout = Workout.query.get(workout_id)
+    if workout:
+        print(f"Found workout: {workout.serialize()}")
+        return jsonify(workout.serialize()), 200
+    else:
+        print("Workout not found")
+        return jsonify({'message': 'workout not retrieved'}), 404
 
-@workouts_blueprint.route('/api/workouts', methods=['POST'])
+
+@workouts_blueprint.route('/api/createworkout', methods=['POST'])
 def create_workout():
     data = request.json
+    print(data)
     user_id = data['user_id']
     date = data['date']
     time = data.get('time')
@@ -23,13 +36,13 @@ def create_workout():
     db.session.commit()
     return jsonify({'message': 'Workout created successfully'})
 
-@workouts_blueprint.route('/api/workouts', methods=['GET'])
+@workouts_blueprint.route('/api/getexercise', methods=['GET'])
 def get_exercises():
     # Retrieve all exercises
     exercises = Exercise.query.all()
     return jsonify([exercise.serialize() for exercise in exercises])
 
-@workouts_blueprint.route('/api/workouts', methods=['POST'])
+@workouts_blueprint.route('/api/createexercise', methods=['POST'])
 def create_exercise():
     data = request.json
     workout_id = data['workout_id']
